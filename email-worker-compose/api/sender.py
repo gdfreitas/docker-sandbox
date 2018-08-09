cb1a69ba82fb8b09fb1af9a5fe7d5f13d1d78d24
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2018-08-09 17:40:00 -0300

    flexibilizado a api de sender e worker para pegar as variaveis de ambientes do compose ou valores padrões

--- email-worker-compose/api/sender.py
+++ email-worker-compose/api/sender.py
@@ -1,6 +1,7 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 class Sender(Bottle):
@@ -8,9 +9,14 @@ class Sender(Bottle):
    def __init__(self):
       super().__init__()
       self.route('/', method='POST', callback=self.send)
-      self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-      DSN = 'dbname=email_sender user=postgres host=db'
-      self.connection = psycopg2.connect(DSN);
+      redis_host = os.getenv('REDIS_HOST', 'queue')
+      self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+
+      db_host = os.getenv('DB_HOST', 'db')
+      db_user = os.getenv('DB_USER', 'postgres')
+      db_name = os.getenv('DB_NAME', 'sender')
+      dsn = f'dbname={db_name} user={db_user} host={db_host}'
+      self.connection = psycopg2.connect(dsn);
 
    def register_message(self, assunto, mensagem):
       SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -1,10 +1,12 @@
 import redis
 import json
+import os
 from time import sleep
 from random import randint
 
 if __name__ == '__main__':
-   r = redis.Redis(host='queue', port=6379, db=0)
+   redis_host = os.getenv('REDIS_HOST', 'queue')
+   r = redis.Redis(host=redis_host, port=6379, db=0)
    print('Aguardando mensagens...')
    while True:
       mensagem = json.loads(r.blpop('sender')[1])
